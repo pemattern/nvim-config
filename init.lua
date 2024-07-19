72c878cb2ade2d44a96225c566093e18ed0667ff
Paul Mattern <pemattern@proton.me>
2024-07-19 12:39:19 +0200

    cleaned up

--- init.lua
+++ init.lua
@@ -9,7 +9,16 @@ vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
 vim.opt.fillchars = { eob = " " }
+
+vim.o.cursorline = true
+
 vim.api.nvim_set_hl(0, 'MyLineNr', { ctermfg = 7 })
 vim.cmd [[highlight! link LineNr MyLineNr]]
+
+vim.api.nvim_set_hl(0, 'MyCursorLineNr', { ctermbg = 15, ctermfg = 8 })
+vim.cmd [[highlight! link CursorLineNr MyCursorLineNr]]
+
+vim.api.nvim_set_hl(0, 'MyCursorLine', { reverse = true })
+vim.cmd [[highlight! link CursorLine MyCursorLine]]
 
 require("config.lazy")
