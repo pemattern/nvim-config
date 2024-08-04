ae9f52c9f853594d55a169792f2b4094df77dfd7
Paul Mattern <pemattern@proton.me>
2024-08-04 21:24:07 +0200

    improved scope visuals

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,8 +1,13 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   opts = {
-    indent = { char = "▏" },
-    scope = { enabled = false, show_start = false, show_end = false },
+    indent = { char = "│", highlight = "Comment" },
+    scope = { char = "│", highlight = "Special", show_start = false, show_end = false },
   },
   main = "ibl",
+  config = function(_, opts)
+    local hooks = require "ibl.hooks"
+    hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+    require("ibl").setup(opts)
+  end,
 }
