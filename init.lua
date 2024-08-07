256c64ce7038b626341ee86e0dfb5e92ce47cb10
Paul Mattern <pemattern@proton.me>
2024-08-07 21:00:52 +0200

    removed tab bar, added statusbar

--- init.lua
+++ init.lua
@@ -10,6 +10,7 @@ vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
 vim.opt.fillchars = { eob = " " }
+vim.opt.cmdheight = 0
 vim.o.cursorline = true
 
 -- set keybinds
@@ -20,11 +21,10 @@ vim.api.nvim_set_hl(0, 'Normal', { ctermfg = 15 })
 vim.api.nvim_set_hl(0, 'LineNr', { ctermfg = 7 })
 vim.api.nvim_set_hl(0, 'CursorLineNr', { ctermbg = 15, ctermfg = 8 })
 vim.api.nvim_set_hl(0, 'CursorLine', { reverse = true })
-
 vim.api.nvim_set_hl(0, 'PMenu', { ctermfg = 15, ctermbg = 4 })
 vim.api.nvim_set_hl(0, 'PMenuSel', { ctermbg = 8, underline = false })
 vim.api.nvim_set_hl(0, 'PMenuThumb', { ctermbg = 12, ctermfg = 4 })
-
+vim.api.nvim_set_hl(0, 'StatusLineNC', { underline = false })
 vim.api.nvim_set_hl(0, 'Keyword', { ctermfg = 13 })
 vim.api.nvim_set_hl(0, 'String', { ctermfg = 10 })
 vim.api.nvim_set_hl(0, 'Number', { ctermfg = 13 })
